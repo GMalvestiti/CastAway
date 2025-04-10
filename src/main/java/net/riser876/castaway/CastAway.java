8cf01bc486491db44911fce8b2562a5fe01352fe
GMalvestiti <gustavo.smalvestiti@gmail.com>
2025-04-09 21:21:49 -0300

    [feat] mod id and logging
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

--- src/main/java/net/riser876/castaway/CastAway.java
+++ src/main/java/net/riser876/castaway/CastAway.java
@@ -2,9 +2,16 @@ package net.riser876.castaway;
 
 import net.fabricmc.api.ModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CastAway implements ModInitializer {
+
+    public static final String MOD_ID = "castaway";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        CastAway.LOGGER.info("[CastAway] Mod loaded.");
     }
 }
