075b23b5ab317e3f6023e7ada0f888db8f488519
GMalvestiti <gustavo.smalvestiti@gmail.com>
2025-04-09 21:17:23 -0300

    [feat] Initial commit
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ src/main/java/net/riser876/castaway/CastAway.java
@@ -0,0 +1,10 @@
+package net.riser876.castaway;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CastAway implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
